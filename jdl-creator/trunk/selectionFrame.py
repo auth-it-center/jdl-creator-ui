aa775ec2de4198866c7bba3b3cba98387cb50daa
kaltirim <kaltirim@lad.grid.auth.gr>
2014-03-22 22:44:35 +0200

    Fixed some names and the placement of the options in the frame

--- jdl-creator/trunk/selectionFrame.py
+++ jdl-creator/trunk/selectionFrame.py
@@ -9,22 +9,22 @@ class selectionFrame(Frame):
 	def __init__(self, master = None):
 		Frame.__init__(self,master)
 		self.initializationProcess()
-		self.populateGrid()
+		self.populateFrame()
 			
 	def initializationProcess(self):
 		self.master.wm_title("Selection Frame")
-		self.grid(row=0)
+		self.grid()
 		
-	def populateGrid(self):
+	def populateFrame(self):
 		categories = self.getCategories()
 		c = 0
 		for category in categories:
-			label = Label(self, text=category[3:-1], font=("Arial", 16)).grid(row = 0, column = c)
+			label = Label(self, text=category[3:-1], font=("Arial", 16)).grid(row = 0, column = c, ipadx=15)
 			category_options = self.getOptions(category)
 			r = 1
 			for option in category_options:
 				var = IntVar()
-				label = Checkbutton(self, text=option, font=("Arial", 14), variable = var, command = self.test).grid(row = r, column = c)
+				check = Checkbutton(self, text=option, font=("Arial", 14), variable = var, command = self.test).grid(row = r, column = c)
 				self.checks.append(var)
 				r = r + 1
 			c = c + 1
